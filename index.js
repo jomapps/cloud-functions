faf7223bcb2a7f66a82d701af7b9f95caaa373f8
Jom Apps Services <65528547+jomapps@users.noreply.github.com>
2023-05-14 02:47:35 +0800

    Added axios

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+const axios = require('axios');
+
 exports.CloudFunctions = (req,res)=> {
-    AMICUS_LOCKBOX_URL = process.env.AMICUS_LOCKBOX_URL;
-    res.send("Hello from TeaCii Cloud Functions! the AMICUS_LOCKBOX_URL is " + AMICUS_LOCKBOX_URL + "");
+    const AMICUS_LOCKBOX_URL = process.env.AMICUS_LOCKBOX_URL;
+    const ID = 176;
+    const URL = `${AMICUS_LOCKBOX_URL}${ID}`;
+
+    try {
+        const response = await axios.get(URL);
+        res.status(200).send("SUCCESS: ".response.data);
+    } catch (error) {
+        res.status(500).send("ERROR: ".error.toString());
+    }
 }
