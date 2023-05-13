bdf881fa965cccc053b440668ed42af9f7ed8a27
Jom Apps Services <65528547+jomapps@users.noreply.github.com>
2023-05-14 02:04:51 +0800

    display amics

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 exports.CloudFunctions = (req,res)=> {
     AMICUS_LOCKBOX_URL = process.env.AMICUS_LOCKBOX_URL;
-    res.send("Hello from TeaCii Cloud Functions!");
+    res.send("Hello from TeaCii Cloud Functions! the AMICUS_LOCKBOX_URL is " + AMICUS_LOCKBOX_URL + "");
 }
