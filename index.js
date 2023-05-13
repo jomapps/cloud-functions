00c980df5a8ce4c3809a56416aa4d30a781226f2
Jom Apps Services <65528547+jomapps@users.noreply.github.com>
2023-05-14 03:12:50 +0800

    chaned with axios

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const axios = require('axios');
 
-exports.CloudFunctions = (req,res)=> {
+exports.CloudFunctions = (req, res) => {
     const AMICUS_LOCKBOX_URL = process.env.AMICUS_LOCKBOX_URL;
     const ID = 176;
     const URL = `${AMICUS_LOCKBOX_URL}${ID}`;
 
     try {
-        const response = await axios.get(URL);
-        res.status(200).send("SUCCESS: ".response.data);
-    } catch (error) {
-        res.status(500).send("ERROR: ".error.toString());
+        axios.get(URL)
+        .then(response=> {
+            if (response.status === 200) {
+                res.status(200).send("SUCCESS: "+response.data);
+            } else {
+                throw new Error(response.data);
+            }
+        })
+    }
+    catch(err) {
+        res.status(500).send("ERROR: "+err.message);
     }
 }
